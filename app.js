411c3f6985a12588e117c8a6bf974187a26ea43d
ic3surf3r <46877492+ic3surf3r@users.noreply.github.com>
2023-04-06 02:27:20 +0200

    Swapping from xhr to fetch and adding joke on load

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 document.getElementById("randomjoke").addEventListener("click", getDadJoke);
+document.addEventListener("DOMContentLoaded", getDadJoke);
+const display = document.getElementById("display");
 
-function getDadJoke(e) {
-  console.log("get joke");
+// function getDadJoke(e) {
+//   console.log("get joke");
 
-  const xhr = new XMLHttpRequest();
+//   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://icanhazdadjoke.com/", true);
+//   xhr.open("GET", "https://icanhazdadjoke.com/", true);
 
-  xhr.setRequestHeader("Accept", "application/json");
+//   xhr.setRequestHeader("Accept", "application/json");
 
-  xhr.onload = function () {
-    let output = "";
-    if (this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      output = `<h1>${response.joke}</h1>`;
-    }
-    document.getElementById("display").innerHTML = output;
-  };
+//   xhr.onload = function () {
+//     let output = "";
+//     if (this.status === 200) {
+//       const response = JSON.parse(this.responseText);
+//       output = `<h1>${response.joke}</h1>`;
+//     }
+//     document.getElementById("display").innerHTML = output;
+//   };
 
-  xhr.send();
+//   xhr.send();
 
-  e.preventDefault();
+//   e.preventDefault();
+// }
+
+async function getDadJoke() {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await response.json();
+  const joke = await data.joke;
+
+  display.innerHTML = `<h1>${joke}</h1>`;
 }
