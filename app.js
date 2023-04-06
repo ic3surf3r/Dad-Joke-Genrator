bb49801daf041d0cddd79b9a521da18c0701ae1e
ic3surf3r <46877492+ic3surf3r@users.noreply.github.com>
2023-04-06 02:34:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,6 @@ document.getElementById("randomjoke").addEventListener("click", getDadJoke);
 document.addEventListener("DOMContentLoaded", getDadJoke);
 const display = document.getElementById("display");
 
-// function getDadJoke(e) {
-//   console.log("get joke");
-
-//   const xhr = new XMLHttpRequest();
-
-//   xhr.open("GET", "https://icanhazdadjoke.com/", true);
-
-//   xhr.setRequestHeader("Accept", "application/json");
-
-//   xhr.onload = function () {
-//     let output = "";
-//     if (this.status === 200) {
-//       const response = JSON.parse(this.responseText);
-//       output = `<h1>${response.joke}</h1>`;
-//     }
-//     document.getElementById("display").innerHTML = output;
-//   };
-
-//   xhr.send();
-
-//   e.preventDefault();
-// }
-
 async function getDadJoke() {
   const response = await fetch("https://icanhazdadjoke.com/", {
     method: "GET",
